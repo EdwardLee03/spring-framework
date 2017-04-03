6ef617b3d0a44f5b655878a321d90f8d05183c5a
Edward Lee <bert825_work@163.com>
2017-04-03 11:22:24 +0800

    [test-应用上下文定制者] ContextCustomizer、ContextCustomizerFactory

--- spring-test/src/main/java/org/springframework/test/context/ContextCustomizer.java
+++ spring-test/src/main/java/org/springframework/test/context/ContextCustomizer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.test.context;
 
@@ -22,6 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
  * Strategy interface for customizing {@link ConfigurableApplicationContext
  * application contexts} that are created and managed by the <em>Spring
  * TestContext Framework</em>.
+ * 定制应用上下文的策略接口。
  *
  * <p>Customizers are created by {@link ContextCustomizerFactory} implementations.
  *
@@ -35,6 +21,7 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @see ContextCustomizerFactory
  * @see org.springframework.test.context.support.AbstractContextLoader#customizeContext
  */
+// 核心接口 应用上下文定制者
 public interface ContextCustomizer {
 
 	/**
--- spring-test/src/main/java/org/springframework/test/context/ContextCustomizerFactory.java
+++ spring-test/src/main/java/org/springframework/test/context/ContextCustomizerFactory.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.test.context;
 
@@ -24,23 +9,28 @@ import java.util.List;
  * <p>Factories are invoked after {@link ContextLoader ContextLoaders} have
  * processed context configuration attributes but before the
  * {@link MergedContextConfiguration} is created.
+ * 其在上下文加载器({@link ContextLoader})处理上下文配置属性之后被调用，
+ * 但在合并的上下文配置({@link MergedContextConfiguration})被创建之前调用。
  *
  * <p>By default, the Spring TestContext Framework will use the
  * {@link org.springframework.core.io.support.SpringFactoriesLoader SpringFactoriesLoader}
  * mechanism for loading factories configured in all {@code META-INF/spring.factories}
  * files on the classpath.
+ * 默认情况下，测试上下文框架会使用Spring工厂加载器({@link org.springframework.core.io.support.SpringFactoriesLoader SpringFactoriesLoader})
+ * 机制来加载在所有{@code META-INF/spring.factories}文件中配置的工厂组件列表。
  *
  * @author Phillip Webb
  * @author Sam Brannen
  * @since 4.3
  */
+// 核心接口 创建应用上下文定制者的工厂
 public interface ContextCustomizerFactory {
 
 	/**
 	 * Create a {@link ContextCustomizer} that should be used to customize a
 	 * {@link org.springframework.context.ConfigurableApplicationContext ConfigurableApplicationContext}
 	 * before it is refreshed.
-	 * @param testClass the test class
+	 * @param testClass the test class (测试类实例)
 	 * @param configAttributes the list of context configuration attributes for
 	 * the test class, ordered <em>bottom-up</em> (i.e., as if we were traversing
 	 * up the class hierarchy); never {@code null} or empty
