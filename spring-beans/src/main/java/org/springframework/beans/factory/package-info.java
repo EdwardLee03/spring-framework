685abdf983f52d1e982e37c3bf20f0736335a609
Edward Lee <bert825_work@163.com>
2019-02-01 22:16:10 +0800

    core包实现Spring的轻量级控制反转容器（Inversion of Control (IoC)）

--- spring-beans/src/main/java/org/springframework/beans/factory/package-info.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/package-info.java
@@ -1,12 +1,16 @@
 /**
  * The core package implementing Spring's lightweight Inversion of Control (IoC) container.
+ * core包实现Spring的轻量级控制反转容器（Inversion of Control (IoC)）。
  *
  * <p>Provides an alternative to the Singleton and Prototype design
  * patterns, including a consistent approach to configuration management.
  * Builds on the org.springframework.beans package.
+ * 提供单例和原型设计模式的替代方案，包括一致的配置管理方法。
+ * 构建在beans包之上
  *
  * <p>This package and related packages are discussed in Chapter 11 of
  * <a href="http://www.amazon.com/exec/obidos/tg/detail/-/0764543857/">Expert One-On-One J2EE Design and Development</a>
  * by Rod Johnson (Wrox, 2002).
+ * 《Expert One-On-One J2EE Design and Development-专家一对一J2EE设计和开发》
  */
 package org.springframework.beans.factory;
