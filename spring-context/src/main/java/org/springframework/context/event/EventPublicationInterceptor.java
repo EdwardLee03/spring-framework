1cb2fc6bcf7f25f61aa0ddaa1d451bb0ada4abdb
Edward Lee <bert825_work@163.com>
2019-08-09 00:54:57 +0800

    [Context] 应用事件发布拦截器：EventPublicationInterceptor implements MethodInterceptor, ApplicationEventPublisherAware

--- spring-context/src/main/java/org/springframework/context/event/EventPublicationInterceptor.java
+++ spring-context/src/main/java/org/springframework/context/event/EventPublicationInterceptor.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2012 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.springframework.context.event;
 
@@ -31,6 +16,7 @@ import org.springframework.context.ApplicationEventPublisherAware;
  * {@code ApplicationEvent} to all {@code ApplicationListeners}
  * registered with an {@code ApplicationEventPublisher} after each
  * <i>successful</i> method invocation.
+ * 应用事件发布拦截器，发布一个应用事件到所有的应用事件监听者列表的方法拦截器。
  *
  * <p>Note that this interceptor is only capable of publishing <i>stateless</i>
  * events configured via the
@@ -48,13 +34,16 @@ import org.springframework.context.ApplicationEventPublisherAware;
 public class EventPublicationInterceptor
 		implements MethodInterceptor, ApplicationEventPublisherAware, InitializingBean {
 
+    /** 应用事件类型的构造器 */
 	private Constructor<?> applicationEventClassConstructor;
 
+    /** 应用事件发布者 */
 	private ApplicationEventPublisher applicationEventPublisher;
 
 
 	/**
 	 * Set the application event class to publish.
+     * 设置要发布的应用事件类型。
 	 * <p>The event class <b>must</b> have a constructor with a single
 	 * {@code Object} argument for the event source. The interceptor
 	 * will pass in the invoked object.
@@ -65,11 +54,12 @@ public class EventPublicationInterceptor
 	public void setApplicationEventClass(Class<?> applicationEventClass) {
 		if (ApplicationEvent.class == applicationEventClass ||
 			!ApplicationEvent.class.isAssignableFrom(applicationEventClass)) {
+		    // 非应用事件子类
 			throw new IllegalArgumentException("applicationEventClass needs to extend ApplicationEvent");
 		}
 		try {
 			this.applicationEventClassConstructor =
-					applicationEventClass.getConstructor(new Class<?>[] {Object.class});
+					applicationEventClass.getConstructor(Object.class);
 		}
 		catch (NoSuchMethodException ex) {
 			throw new IllegalArgumentException("applicationEventClass [" +
@@ -84,18 +74,23 @@ public class EventPublicationInterceptor
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
+        // 必要条件检查
 		if (this.applicationEventClassConstructor == null) {
 			throw new IllegalArgumentException("applicationEventClass is required");
 		}
 	}
 
-
+    /**
+     * 在调用方法之后发布应用事件。
+     */
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
+	    // 处理链中的下一个拦截器
 		Object retVal = invocation.proceed();
 
 		ApplicationEvent event = (ApplicationEvent)
-				this.applicationEventClassConstructor.newInstance(new Object[] {invocation.getThis()});
+				this.applicationEventClassConstructor.newInstance(invocation.getThis());
+        // 通知所有匹配的注册了这个应用事件的监听者列表
 		this.applicationEventPublisher.publishEvent(event);
 
 		return retVal;
