fd4eb8d8f4b7e916b4df5182481c77233d467f30
Edward Lee <bert825_work@163.com>
2019-08-09 00:49:15 +0800

    [AOP] 行为连接点之方法调用：Joinpoint, Invocation, MethodInvocation

--- spring-aop/src/main/java/org/aopalliance/intercept/Joinpoint.java
+++ spring-aop/src/main/java/org/aopalliance/intercept/Joinpoint.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package org.aopalliance.intercept;
 
@@ -21,18 +6,22 @@ import java.lang.reflect.AccessibleObject;
 /**
  * This interface represents a generic runtime joinpoint (in the AOP
  * terminology).
+ * 表示通用运行时的连接点(AOP术语)。
  *
  * <p>A runtime joinpoint is an <i>event</i> that occurs on a static
  * joinpoint (i.e. a location in a the program). For instance, an
  * invocation is the runtime joinpoint on a method (static joinpoint).
  * The static part of a given joinpoint can be generically retrieved
  * using the {@link #getStaticPart()} method.
+ * 运行时连接点是在静态连接点(程序中的位置)上发生的事件。
  *
  * <p>In the context of an interception framework, a runtime joinpoint
  * is then the reification of an access to an accessible object (a
  * method, a constructor, a field), i.e. the static part of the
  * joinpoint. It is passed to the interceptors that are installed on
  * the static joinpoint.
+ * 在拦截框架的上下文中，运行时连接点是可访问的对象(方法，构造函数，字段)的访问的具体化。
+ * 它被传递给安装在静态连接点的拦截器。
  *
  * @author Rod Johnson
  * @see Interceptor
@@ -41,6 +30,7 @@ public interface Joinpoint {
 
 	/**
 	 * Proceed to the next interceptor in the chain.
+	 * 处理调用链中的下一个拦截器。
 	 * <p>The implementation and the semantics of this method depends
 	 * on the actual joinpoint type (see the children interfaces).
 	 * @return see the children interfaces' proceed definition
@@ -50,6 +40,7 @@ public interface Joinpoint {
 
 	/**
 	 * Return the object that holds the current joinpoint's static part.
+	 * 返回保存当前连接点的静态部分的对象。
 	 * <p>For instance, the target object for an invocation.
 	 * @return the object (can be null if the accessible object is static)
 	 */
@@ -57,6 +48,7 @@ public interface Joinpoint {
 
 	/**
 	 * Return the static part of this joinpoint.
+	 * 返回这个连接点的静态部分。
 	 * <p>The static part is an accessible object on which a chain of
 	 * interceptors are installed.
 	 */
